ddbe5820dcca4488a789d255e1a7cde65a5ceaa5
Jordan Stomps <stomps@wisc.edu>
2020-05-12 14:52:49 -0500

    Moving possible functions to seperate branch

--- writer.py
+++ writer.py
@@ -4,6 +4,21 @@ import h5py
 import os
 
 
+def make_group(hdf5_file, group_name):
+    hdf5_file.create_group(group_name)
+
+
+def init_dataset(subgrp_name, subgroup, column):
+    return subgroup.create_dataset(subgrp_name,
+                                   column.shape,
+                                   dtype=column.dtypes)
+
+
+def write_column(group, subgroup):
+    for row in enumerate(group[subgroup]):
+        group[row[0]] = row[1]
+
+
 def main():
     # input parameters
     data_directory = 'AoT_Chicago.complete.2020-05-04/'
